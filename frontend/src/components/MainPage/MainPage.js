7af91f88819bcd7342e07420fb89e3068f7beedc
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-24 12:15:33 -0400

    Rearrage some divs

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,25 +1,28 @@
 import "./MainPage.css";
 
 function MainPage() {
-  return (
-    <div id="main-page">
-        <div id="main-page-top">
-            <div id="main-page-top-left">
+    return (
+        <div id="main-page">
+            <div id="main-page-left">
                 <div id="cubberd-search-container" className="main-page-component"></div>
             </div>
-            <div id="main-page-top-right">
-                <div id="navbar-container" className="main-page-component"></div>
-                <div id="main-page-top-right-bottom">
-                    <div id="pot-container" className="main-page-component"></div>
-                    <div id="shopping-list-container" className="main-page-component"></div>
+            <div id="main-page-right">
+            <div id="main-page-top">
+                <div id="main-page-top-right">
+                    <div id="navbar-container" className="main-page-component"></div>
+                    <div id="main-page-top-right-bottom">
+                        <div id="pot-container" className="main-page-component"></div>
+                        <div id="shopping-list-container" className="main-page-component"></div>
+                    </div>
                 </div>
             </div>
+            <div id="main-page-bottom">
+                <div id="current-cubberd-container" className="main-page-component"></div>
+            </div>
+
+            </div>
         </div>
-        <div id="main-page-bottom">
-            <div id="current-cubberd-container" className="main-page-component"></div>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default MainPage;
