0975a1d0df1ac02c42b7a0269037550189a07f2b
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-24 12:00:49 -0400

    Change from fixed position to flex elements

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -2,12 +2,22 @@ import "./MainPage.css";
 
 function MainPage() {
   return (
-    <div id="Main-page">
-      <div id="navbar-container" className="main-page-component"></div>
-      <div id="cubberd-search-container" className="main-page-component"></div>
-      <div id="pot-container" className="main-page-component"></div>
-      <div id="current-cubberd-container" className="main-page-component"></div>
-      <div id="shopping-list-container" className="main-page-component"></div>
+    <div id="main-page">
+        <div id="main-page-top">
+            <div id="main-page-top-left">
+                <div id="cubberd-search-container" className="main-page-component"></div>
+            </div>
+            <div id="main-page-top-right">
+                <div id="navbar-container" className="main-page-component"></div>
+                <div id="main-page-top-right-bottom">
+                    <div id="pot-container" className="main-page-component"></div>
+                    <div id="shopping-list-container" className="main-page-component"></div>
+                </div>
+            </div>
+        </div>
+        <div id="main-page-bottom">
+            <div id="current-cubberd-container" className="main-page-component"></div>
+        </div>
     </div>
   );
 }
