4f5cb4ba62b676e3257e36dbe68b0b4633838971
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-24 16:27:17 -0400

    create file

---
+++ frontend/src/components/SessionFormModals/LoginFormModal.js
@@ -0,0 +1,13 @@
+import { Modal } from '../../context/Modal';
+import React from 'react';
+import LoginForm from '../SessionForms/LoginForm';
+
+
+function LoginFormModal() {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <>
+        </>
+    )
+}
