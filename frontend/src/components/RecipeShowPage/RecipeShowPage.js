b9d71bf04020c36e08e6dcca250111945535e4b7
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-26 10:48:00 -0400

    add overflow to relevant flex-boxes

--- frontend/src/components/RecipeShowPage/RecipeShowPage.js
+++ frontend/src/components/RecipeShowPage/RecipeShowPage.js
@@ -1,6 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRecipe } from "../../store/recipes";
+import { useEffect } from "react";
 
 function RecipeShowPage() {
+    const { id } = useParams();
+    const recipe = useSelector(state => state.recipes);
 
+    // useEffect(() => {
+    //     dispatchEvent(fetchRecipe(id));
+    // }, [])
+
+    return (
+        <h1>recipe show page {id} </h1>
+    )
 }
 
 export default RecipeShowPage;
--- frontend/src/store/recipes.js
+++ frontend/src/store/recipes.js
@@ -9,6 +9,11 @@ const receiveRecipes = (recipes) => ({
     recipes
 })
 
+const receiveRecipe = (recipe) => ({
+    type: RECEIVE_RECIPE,
+    recipe
+})
+
 export const fetchRecipes = () => async (dispatch) => {
     const res = await jwtFetch("/api/recipes");
     const recipes = await res.json();
@@ -16,10 +21,19 @@ export const fetchRecipes = () => async (dispatch) => {
 }
 
 
+export const fetchRecipe = (id) => async (dispatch) => {
+    const res = await jwtFetch(`/api/recipes/${id}`)
+    const recipe = await res.json();
+    dispatch(receiveRecipe(recipe));
+}
+
+
 const recipesReducer = (state = {}, action) => {
     switch (action.type) {
         case RECEIVE_RECIPES:
             return {...state, ...action.recipes}
+        case RECEIVE_RECIPE:
+            return {...state, ...action.recipe}
         default:
             return state;
     }
