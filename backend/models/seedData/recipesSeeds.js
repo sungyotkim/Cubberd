3f1f7a04b356927486d2d6ced23483e654c56a03
Frederick <fred.chien3@gmail.com>
2022-10-26 15:02:43 -0400

    Add images to Ingredients that don't have one

--- backend/models/seedData/recipesSeeds.js
+++ backend/models/seedData/recipesSeeds.js
@@ -2,7 +2,6 @@ require('dotenv').config({path: '../../.env'});
 
 const mongoose = require('mongoose');
 const {mongoURI: db} = require('../../config/keys.js');
-
 const Ingredient = require('../Ingredient');
 const Recipe = require('../Recipe');
 const recipeArr = require('./recipeArr');
@@ -25,29 +24,90 @@ const nestedRecipes = recipeArr;
 const seedRecipes = [];
 let seedIngredientsHash = {};
 
+const capitalizeName = (string) => {
+  const foodNameArr = string.split(" ");
+  for (let i = 0; i < foodNameArr.length; i++) {
+    foodNameArr[i] = foodNameArr[i][0].toUpperCase() + foodNameArr[i].substr(1);
+  }    
+  return foodNameArr.join(" ");
+}
+
 nestedRecipes.forEach(nestedRecipe => {
   const recipeObject = nestedRecipe.recipe;
-  seedRecipes.push(recipeObject);
 
   recipeObject.ingredients.forEach(ingredient => {
-    const foodNameArr = ingredient.food.split(" ");
-
-    for (let i = 0; i < foodNameArr.length; i++) {
-      foodNameArr[i] = foodNameArr[i][0].toUpperCase() + foodNameArr[i].substr(1);
-    }    
-
-    const newFoodName = foodNameArr.join(" ");
+    
+    const newFoodName = capitalizeName(ingredient.food);
     const newIngredient = {
       food: newFoodName,
       foodCategory: ingredient.foodCategory,
       image: ingredient.image,
       foodId: ingredient.foodId
     };
+    if (newIngredient.image === null) {
+      switch (newIngredient.food) {
+        case "Rendered Chicken Fat": 
+        newIngredient.image = "https://images.heb.com/is/image/HEBGrocery/000168024";
+        case "Crushed Tomatoes":
+          newIngredient.image = "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRhL5bzu6VoYW5bV8G2M8UXjP595VkuxaCJgJDpTo6TDbanJS74beZFVTQQ7x3L-ZaOFYuDeczVSHPEfl5CgmQEDc1SDDpA&usqp=CAE";
+        case "Polenta":
+          newIngredient.image = "https://cdn.loveandlemons.com/wp-content/uploads/2020/02/polenta.jpg";
+        case "Pam":
+          newIngredient.image = "https://m.media-amazon.com/images/I/51ihv1zrAmS.jpg";
+        case "Tomato Sauce":
+          newIngredient.image = "https://www.seriouseats.com/thmb/rlK8y8KJegOzRYYDpwlr-yDydc8=/1500x1125/filters:fill(auto,1)/the-best-slow-cooked-italian-american-tomato-sauce-red-sauce-recipe-step-09-05ad7578f9d34a5092086ae959bea8a7.jpg";
+        case "Ground Flax":
+          newIngredient.image = "https://m.media-amazon.com/images/I/61IIIPUDd1L.jpg";
+        case "Oats":
+          newIngredient.image = "https://cdn1.sph.harvard.edu/wp-content/uploads/sites/30/2018/03/oats-701299_1920.jpg";
+        case "Nutritional Yeast":
+          newIngredient.image = "https://cdn.loveandlemons.com/wp-content/uploads/2021/04/what-is-nutritional-yeast.jpg";
+        case "Dried Mustard":
+          newIngredient.image = "https://www.myspicer.com/wp-content/uploads/2014/12/uses-for-dry-mustard.png";
+        case "Winter Squash":
+          newIngredient.image = "https://assets.epicurious.com/photos/5893bf1aeb3e360a3e384090/master/w_2000,h_1333,c_limit/butternut-squash-020217.jpg";
+        case "Asafetida":
+          newIngredient.image = "https://m.media-amazon.com/images/I/71IvC4hwz7L.jpg";
+        case "Chat Masala":
+          newIngredient.image = "https://m.media-amazon.com/images/I/716mHetqbGL.jpg";
+        case "Seitan":
+          newIngredient.image = "https://www.connoisseurusveg.com/wp-content/uploads/2015/07/how-to-make-seitan-9-of-10.jpg";
+        case "Turkey Necks":
+          newIngredient.image = "https://petparlour.ie/wp-content/uploads/2020/10/Raw-Turkey-Necks-Frozen.jpeg";
+        case "Turkey Fat":
+          newIngredient.image = "https://thumbs.dreamstime.com/b/fat-black-turkey-red-snood-his-head-158360904.jpg";
+        case "https://thumbs.dreamstime.com/b/fat-black-turkey-red-snood-his-head-158360904.jpg":
+          newIngredient.image = "https://m.media-amazon.com/images/I/41PQTi0k7VL.jpg";
+        case "Fruit":
+          newIngredient.image = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assortment-of-colorful-ripe-tropical-fruits-top-royalty-free-image-995518546-1564092355.jpg";
+        case "Coconut Aminos":
+          newIngredient.image = "https://cdn.shopify.com/s/files/1/0242/1866/5056/products/FM199BI341_SVCBOCLA10_IMD.png?v=1639624865";
+        case "Lamb Neck":
+          newIngredient.image = "https://www.shepherdsongfarm.com/wp-content/uploads/2020/12/Sliced-Lamb-Neck-3.jpg";
+        case "Lamb Shanks":
+          newIngredient.image = "https://www.shepherdsongfarm.com/wp-content/uploads/2012/09/Lamb-Shanks-1.jpg";
+        case "Panko":
+          newIngredient.image = "https://m.media-amazon.com/images/I/81fer2Xw7RS.jpg";
+        case "Oxtails":
+          newIngredient.image = "https://www.centralmeats.com/pub/media/catalog/product/cache/image/600x430/e9c3970ab036de70892d86c6d221abfe/o/x/oxtails.jpg";
+        case "Vegetable Broth":
+          newIngredient.image = "http://contenthandler.azureedge.net/recp/477/2000/0/444.jpg";
+        case "Cookie Crumbs":
+          newIngredient.image = "https://associationsnow.com/wp-content/uploads/2018/02/GettyImages-885134872.jpg";
+        case "Jelly":
+          newIngredient.image = "https://media.istockphoto.com/photos/jelly-on-a-plate-3d-rendering-isolated-on-white-background-picture-id1056477498?k=20&m=1056477498&s=170667a&w=0&h=BZnb28BulzFtgW4qXGqGtIuFew9GMGnoNcfw7e8m_JY=";
+        case "Can Of Chickpeas":
+          newIngredient.image = "https://target.scene7.com/is/image/Target/GUEST_aab59635-53ff-4f74-a8b0-603021196498?wid=488&hei=488&fmt=pjpeg";
+        default:
+          newIngredient.image = "https://media-exp1.licdn.com/dms/image/D4E03AQE6zongzg-HUQ/profile-displayphoto-shrink_400_400/0/1666311882598?e=1672272000&v=beta&t=mgQlRPS2tNY5M8r3Lk1A1tJfDKAtGsfo-hbl0AWH7jY";
+      }
+    }
+    seedIngredientsHash[newIngredient.foodId] = newIngredient;
 
-   seedIngredientsHash[newIngredient.foodId] = newIngredient;
+    ingredient.food = newFoodName;
   })
+  seedRecipes.push(recipeObject);
 })
-
 seedIngredients = Object.values(seedIngredientsHash);
 
 
