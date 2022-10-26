f6f5761da96a696573e0112583f6ed74806c89bb
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-26 14:35:26 -0400

    put request on quantity change

--- frontend/src/components/ShoppingListItem/ShoppingListItem.js
+++ frontend/src/components/ShoppingListItem/ShoppingListItem.js
@@ -1,12 +1,26 @@
+import { useState } from "react"
+import { useDispatch } from "react-redux";
+import { editShoppingList } from "../../store/shoppingList";
+import { useSelector } from "react-redux";
 
 function ShoppingListItem({item}) {
+    const sessionUser = useSelector(state => state.session.user);
 
+    const dispatch = useDispatch();
+
+    const [quantity, setQuantity] = useState(item.quantity);
+
+    const handleChange = (e) => {
+        setQuantity(e.target.value);
+
+        const editedItem = item.quantity = quantity;
+        dispatch(editShoppingList(sessionUser._id, editedItem))
+    }
 
 
     return (
         <div className="shoppingListItem">
-            {item.ingredient.food}
-
+            <form><input type="number" onChange={handleChange} value={quantity}></input></form>{item.ingredient.food}
         </div>
     )
 
